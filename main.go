deadc7a46c50885a4d7a669524bad4d2adbd2c1a
adam <a.astamirov@gmail.com>
2026-03-25 10:39:30 +0300

    add simple connection

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"study/feature1"
 	"study/feature2"
+	"study/feature_postgres/simple_connection"
 )
+
+//"postgres://YourUserName:YourPassword@YourHostName:5432/YourDatabaseName"
 
 func main() {
 	fmt.Println("Hello Git")
 	feature1.Feature1()
 	feature2.Feature2()
+	simple_connection.CheckConnection()
 
 }
